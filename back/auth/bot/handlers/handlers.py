f1d9e4f0d4b4a3ee5270802bee941cdbe958b928
pythoneer-dev-q <weqvolohov@gmail.com>
2026-03-01 14:19:30 +0300

    добавил отображение курсов (достпуных пользователю)

--- back/auth/bot/handlers/handlers.py
+++ back/auth/bot/handlers/handlers.py
@@ -21,10 +21,13 @@ async def userReg(call: CallbackQuery):
     await call.message.answer('Регистрация..')
     if await api.set_user_TG(user_id=call.from_user.id, username=call.data.split(':')[1]) == 'success':
         await call.message.edit_text('<b>💡 Главное меню</b>', reply_markup=await kb.main_Keyboard())
+    else:
+        await call.message.edit_text('Регистрация завершилась с ошибками. Попробуйте получить ссылку в профиле.')
 
 @vrouter.message(CommandStart())
 async def main_userCabinet(message: Message):
     await message.answer('<b>💡 Главное меню</b>', reply_markup=await kb.main_Keyboard())
+
 @vrouter.callback_query(F.data.startswith('user_'))
 async def main_functions(call: CallbackQuery):
     match call.data[5:]:
@@ -34,5 +37,7 @@ async def main_functions(call: CallbackQuery):
             await call.message.edit_text(f'<b>🧩 Достижения на платформе</b>\n\n<i>Достижения - важный шаг к познанию и обучению, они разработаны, дабы благодарить учеников за их труды и старания</i>\nНажмите на любое из них и получите по нему информацию\n\n{await api.main_get_userAchievements(call.from_user.id)}')
         case 'courses':
             await call.message.edit_text(f'<b>✅ Доступные курсы</b>\n\n<i>Это курсы, к которым имеет доступ только ваша роль (<b>у вас сейчас: "user"</b>). Отельной роли назначаются отдельные курсы. Посмотрите их ниже.</i>\n\n{await api.main_get_userCourses(call.from_user.id)}')
+        case '':
+            pass
         case _:
             await call.answer('Запретная зона...')
--- back/auth/bot/utils/keyboards.py
+++ back/auth/bot/utils/keyboards.py
@@ -1,13 +1,13 @@
 from aiogram.types import InlineKeyboardMarkup as IKM, InlineKeyboardButton as IKB
-
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 async def main_generateAuthKeyboard(username: str) -> IKM:
     keyboard = IKM(
         inline_keyboard=[
             [IKB(text='🧩 Регистрация в боте NotALMS',
-                 callback_data=f'registration:{username}', style='success')],
+                callback_data=f'registration:{username}', style='success')],
             [IKB(text='❌ Отменить действие',
-                 callback_data='user_cancel_all', style='danger')]
+                callback_data='user_cancel_all', style='danger')]
         ]
     )
     return keyboard
@@ -17,10 +17,23 @@ async def main_Keyboard() -> IKM:
     keyboard = IKM(
         inline_keyboard=[
             [IKB(text='🎉 Мои достижения', callback_data='user_achievements', style='primary'),
-             IKB(text='🔮 Доступные курсы', callback_data='user_courses', style='primary')],
+            IKB(text='🔮 Мои курсы', callback_data='user_courses', style='primary')],
             [IKB(text='⚡️ Новости платформы', callback_data='user_getNews', style='success')],
             [IKB(text='⚒️ Настройки', callback_data='user_settings', style='success'), 
-             IKB(text='🧩 API (разработчики)', callback_data='user_devAPI', style='primary')]
+            IKB(text='🧩 API', callback_data='user_devAPI', style='primary')]
         ]
     )
     return keyboard
+
+async def main_refferer() -> IKM:
+    return IKM(
+        inline_keyboard=[[
+            IKB(text='👤 Перейти в профиль ↱', url='https://lms.notawallet.sbs/profile', style='primary')
+        ]]
+    )
+
+async def main_coursesBuilder(courses_list: list):
+    builder = InlineKeyboardBuilder()
+    for course in courses_list:
+        builder.add(IKB(text=f'📌 - {course.get('title', 'undefined')}', callback_data=f'view_course:{course.get('title', 'undefined')}'))
+    builder.add(IKB(text='↖️ Назад', callback_data='user_courses'))
